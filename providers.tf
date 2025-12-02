9fa724e832aec3e9ca856426dd64f3bc11b43932
cuneguines <47780327+cuneguines@users.noreply.github.com>
2025-12-02 09:46:25 +0000

    Changes in the providers.tf file

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
+# Specify the minimum required Terraform version to run this code.
+# Ensures that anyone using this configuration has a compatible Terraform version
+# with the features used in this code. Here, >= 1.3.0 means Terraform 1.3.0 or higher.
+# The AWS region where resources will be created.
+# It references a Terraform variable (var.aws_region), making this configuration reusable.
+# By using a variable instead of hardcoding, you can deploy to different regions easily.
 terraform {
   required_version = ">= 1.3.0"
 
